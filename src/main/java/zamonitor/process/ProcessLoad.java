4337142a2cfa0a167392824fbea8c2b67af4c8eb
Phil Sampson <psampson@macbook.(none)>
2013-05-12 18:36:13 +0100

    Refactoring

---
+++ src/main/java/zamonitor/process/ProcessLoad.java
@@ -0,0 +1,21 @@
+package zamonitor.process;
+
+
+
+/**
+ * User: zammalad
+ * Date: 12/05/2013
+ * Time: 18:32
+ * Desc:
+ */
+public class ProcessLoad extends ProcessSaveLoad implements Runnable {
+
+    private ProcessLoad() {
+        super();
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
---
+++ src/main/java/zamonitor/process/ProcessSaveLoad.java
@@ -0,0 +1,68 @@
+package zamonitor.process;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * User: psampson
+ * Date: 10/05/2013
+ * Time: 22:12
+ * Desc: Base class for saving or loading ProcessInfo
+ */
+public class ProcessSaveLoad {
+
+    private String fileName;
+    private ConcurrentHashMap<String, ArrayList<ProcessInfo>> stats;
+
+    /**
+     * Get the file name
+     * @return The file name
+     */
+    public String getFileName() {
+        return fileName;
+    }
+
+    /**
+     * Set the file name
+     * @param fileName The file name
+     */
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    /**
+     * Get the map of ProcessInfo
+     * @return The map of ProcessInfo
+     */
+    public ConcurrentHashMap<String, ArrayList<ProcessInfo>> getStats() {
+        return stats;
+    }
+
+    /**
+     * Set the map of ProcessInfo
+     * @param stats The map of ProcessInfo
+     */
+    public void setStats(ConcurrentHashMap<String, ArrayList<ProcessInfo>> stats) {
+        this.stats = stats;
+    }
+
+    protected ProcessSaveLoad() {
+        // Intentionally private and empty
+    }
+
+    public ProcessSaveLoad(ConcurrentHashMap<String, ArrayList<ProcessInfo>> stats) {
+        this(stats, "ProcessOutput.txt");
+    }
+
+    public ProcessSaveLoad(ConcurrentHashMap<String, ArrayList<ProcessInfo>> stats, String fileName) {
+        this.stats = stats;
+        this.fileName = fileName;
+    }
+}
